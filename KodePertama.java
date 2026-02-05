fe76f4ec58453225ebee4def527deb24987ecca5
TiaraChontesa <d1041231070@student.untan.ac.id>
2026-02-05 17:07:42 +0700

    Delete KodePertama.java

--- KodePertama.java
+++
@@ -1,8 +0,0 @@
-public class KodePertama {
-    public static void main(String[] args) {
-        System.out.println("Nama            : Tiara Chontesa");
-        System.out.println("NIM             : D1041231070");
-        System.out.println("Semester        : 6");
-        System.out.println("Prodi           : Informatika");
-    }
-}
